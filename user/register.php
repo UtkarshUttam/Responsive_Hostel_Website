9da804be14a7eddb7bf18405318b64e048a9f6d0
Utkarsh Uttam <utkarshuttam2044@gmail.com>
2022-09-05 18:50:18 +0530

    Added login & register
    added php file for login and register backend

---
+++ user/register.php
@@ -0,0 +1,60 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="../css/login_page_style.css">
+</head>
+<body>
+<nav class="navbar nav-dark">
+        <ul class="nav-list">
+            <div class="logo"><img src="../media/logo.png" alt="logo" style="border:3px solid white; border-radius:50px">
+            </div>
+            <li><a href="../home.html">Home</a></li>
+            <li><a href="../about.html">About</a></li>
+            <li><a href="../services/services.html">Services</a></li>
+            <li><a href="../contact.html">Contact Us</a></li>
+        </ul>
+        <div class="rightNav">
+            <input type="text" name="search" id="search" placeholder="Hola!">
+            <button class="btn btn-sm">Search</button>
+        </div>
+    </nav>
+<section class="login-background">
+	<div class="login-box">
+		<h2>Register</h2>
+		<form method="post" action="register.php">
+			<?php include('errors.php'); ?>
+			<div class="user-box">
+				<input type="text" name="reg_no" value="<?php echo $reg_no; ?>">
+				<label>Registration Number</label>
+			</div>
+			<div class="user-box">
+				<input type="text" name="mobile_no" value="<?php echo $mobile_no; ?>">
+				<label>Mobile Number</label>
+			</div>
+			<div class="user-box">
+				<input type="text" name="email" value="<?php echo $email; ?>">
+				<label>Email</label>
+			</div>
+			<div class="user-box">
+				<input type="text" name="username" value="<?php echo $username; ?>">
+				<label>Username</label>
+			</div>
+			<div class="user-box">
+				<input type="password" name="password_1">
+				<label>Password</label>
+			</div>
+			<div class="user-box">
+				<input type="password" name="password_2">
+				<label>Confirm password</label>
+			</div>
+			<div>
+				<button type="submit" class="btn11" name="reg_user">submit</button>
+			</div>
+  	<p style="color:white;">
+  		Already a member? <a href="login.php" style="text-decoration: none; color: coral;">Sign in</a>
+  	</p>
+  </form>
+</body>
+</html>
---
+++ user/server.php
@@ -0,0 +1,90 @@
+<?php
+session_start();
+
+// initializing variables
+$username = "";
+$email    = "";
+$reg_no = "";
+$mobile_no = "";
+$errors = array(); 
+
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'hostel_data');
+
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $reg_no = mysqli_real_escape_string($db, $_POST['reg_no']);
+  $mobile_no = mysqli_real_escape_string($db, $_POST['mobile_no']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($reg_no)) { array_push($errors, "Registration number is required"); }
+  if (empty($mobile_no)) { array_push($errors, "Mobile number is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+  // first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM userinfo WHERE username='$username' OR email_id='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO userinfo (Registration_No, Mobile_No, email_ID, username, password) 
+  			  VALUES( '$reg_no', '$mobile_no', '$email', '$username', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: index.php');
+  }
+}
+
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	$password = md5($password);
+  	$query = "SELECT * FROM userinfo WHERE username='$username' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['username'] = $username;
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: index.php');
+  	}else {
+  		array_push($errors, "Wrong username/password combination");
+  	}
+  }
+}
+
+?>
